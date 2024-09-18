1365416244c70abdd474c9ca9dd47f4711f275fb
Samannoy Bhattacharjee <82638503+SamannoyB@users.noreply.github.com>
2024-09-18 18:33:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
    InitializeLib(ImageHandle, SystemTable);
    Print(L"Hello, world!\n");
-
+   
+   Print(L"Press any key to continue...\n");
+   
+   WaitForSingleEvent(SystemTable->ConIn->WaitForKey, 0);
    return EFI_SUCCESS;
 }
